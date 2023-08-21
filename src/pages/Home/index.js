c86ba1ef54ab87572bb7eafd4529ffd2913fc178
aline-fedorowicz <alinewicz@gmail.com>
2023-08-21 15:57:15 -0300

    create myBank

---
+++ src/pages/Home/index.js
@@ -0,0 +1,72 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from '../../components/Header';
+import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
+import Actions from '../../components/Actions';
+
+const list = [
+  {
+    id:1,
+    label: 'UberTrip',
+    value: '45,00',
+    date: '17/12/2023',
+    type: 0 //despesas
+  },
+
+  {
+    id:2,
+    label: 'Pix Aline',
+    value: '2.500,00',
+    date: '17/12/2023',
+    type: 1 //entradas
+  },
+
+  {
+    id:3,
+    label: 'Salário',
+    value: '10.500,00',
+    date: '18/12/2023',
+    type: 1 //entradas
+  }
+]
+
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Header name="Aline Fedorowicz"/>
+      <Balance saldo="2,45" gastos="-400,00"/>
+      <Actions/>
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+      <FlatList
+
+      style={styles.list}
+      data={list}
+      keyExtrator={ (item => String (item.id))}
+      showsVerticalScrollIndicator={false}
+      renderItem={ ({item}) => <Movements data={item}/> }
+      
+      />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#454546',
+  },
+  title:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 14,
+  },
+
+  list:{
+    marginStart: 14,
+    marginEnd: 14,
+
+  }
+});
